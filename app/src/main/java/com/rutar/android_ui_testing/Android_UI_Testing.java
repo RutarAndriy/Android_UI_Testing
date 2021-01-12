abefe2bf65c76a5abf17255be8188de4679a82ae
RutarAndriy <RutarAndriy@gmail.com>
2021-01-12 16:20:02 +0200

    Project template generated

---
+++ app/src/main/java/com/rutar/android_ui_testing/Android_UI_Testing.java
@@ -0,0 +1,14 @@
+package com.rutar.android_ui_testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Android_UI_Testing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
